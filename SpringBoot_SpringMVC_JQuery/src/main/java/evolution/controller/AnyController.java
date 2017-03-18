a1f10ada3edd21cfb7724a672a4fc740e5bb1885
fslic <fslic@DESKTOP-MCATCHJ>
2017-03-18 11:21:51 +0800

    refactor

--- SpringBoot_SpringMVC_JQuery/src/main/java/evolution/controller/AnyController.java
+++ SpringBoot_SpringMVC_JQuery/src/main/java/evolution/controller/AnyController.java
@@ -2,7 +2,7 @@ package evolution.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +15,15 @@ import lombok.extern.slf4j.Slf4j;
 public class AnyController {
 	// @RequestBody converts JSON to Java DTO.
 	// The implicit @ResponseBody in @RestController converts Java DTO to JSON.
-	@PostMapping(value = "/post", produces = {MediaType.APPLICATION_JSON_VALUE})
+	@PostMapping(value = "/post")
 	public AnyDto anyMethod(@RequestBody AnyDto anyDto, HttpServletRequest request) {
 		log.info(anyDto.toString());
+		return anyDto;
+	}
+	
+	@GetMapping("/get")
+	public AnyDto anotherMethod() {
+		AnyDto anyDto = new AnyDto("Chen", "M");
 		return anyDto;
 	}
 }
--- SpringBoot_SpringMVC_JQuery/src/main/java/evolution/dto/AnyDto.java
+++ SpringBoot_SpringMVC_JQuery/src/main/java/evolution/dto/AnyDto.java
@@ -1,8 +1,12 @@
 package evolution.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AnyDto {
 	private String name;
 	private String gender;
